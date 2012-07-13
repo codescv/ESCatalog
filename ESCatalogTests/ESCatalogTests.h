4d56ea69dd5779f3a8ff35f7e42403cc66bb410f
Chi Zhang <elecpaoao@gmail.com>
2012-07-13 09:14:06 +0800

    Initial Commit

---
+++ ESCatalogTests/ESCatalogTests.h
@@ -0,0 +1,13 @@
+//
+//  ESCatalogTests.h
+//  ESCatalogTests
+//
+//  Created by Zhang Chi on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ESCatalogTests : SenTestCase
+
+@end
---
+++ ESCatalogTests/ESCatalogTests.m
@@ -0,0 +1,32 @@
+//
+//  ESCatalogTests.m
+//  ESCatalogTests
+//
+//  Created by Zhang Chi on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ESCatalogTests.h"
+
+@implementation ESCatalogTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ESCatalogTests");
+}
+
+@end
